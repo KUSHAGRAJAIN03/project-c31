06cb12c5ab4eb46a4c71508d2bd906ccff7a068f
KUSHAGRAJAIN03 <66012878+KUSHAGRAJAIN03@users.noreply.github.com>
2020-07-27 16:35:47 +0530

    Add files via upload

---
+++ division.js
@@ -0,0 +1,24 @@
+class division{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic: true
+
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        World.add(world,this.body);
+
+
+        }
+        display()
+        {
+            var position = this.body.position;
+            rectMode(CENTER);
+            fill(255,255,255);
+            rect(position.x,position.y,this.w,this.h);
+
+
+        }
+    }
+
